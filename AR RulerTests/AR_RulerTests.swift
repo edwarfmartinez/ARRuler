4915581615c6985abb3ca16fdb46195334181208
Edwar Martinez <edwarmartinez@yahoo.com>
2025-08-21 15:29:09 -0500

    Initial Commit

---
+++ AR RulerTests/AR_RulerTests.swift
@@ -0,0 +1,17 @@
+//
+//  AR_RulerTests.swift
+//  AR RulerTests
+//
+//  Created by EDWAR FERNANDO MARTINEZ CASTRO on 21/08/25.
+//
+
+import Testing
+@testable import AR_Ruler
+
+struct AR_RulerTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
